7cc98cdd921c64d5683eb71d0702c8a2fa2a201c
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-02-13 18:40:48 +0100

    update js features - modal,scroll,scroll-animation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initFuncionamento from './modules/horario-funcionamenot.js'
+initFuncionamento()
+
+import scrollSuave from './modules/scroll-suave.js'
+scrollSuave()
+
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+initAnimacaoScroll()
+
+import initmodal from './modules/modal-saibamais.js'
+initmodal()
